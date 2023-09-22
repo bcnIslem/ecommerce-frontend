76126eb14c879b4b487f84b6d6965b3f32d52521
bcnIslem <seifelislem.boucenina@univ-setif.dz>
2023-09-22 07:16:54 +0100

    createdthe categories actions and redux state

---
+++ src/redux/categories.ts
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Define the structure of a category object
+interface Categories {
+  category_id: number;
+  name: string;
+  proprieties: Record<string, any>;
+}
+
+// Define your state interface
+interface CategoriesState {
+  categories: Categories[];
+  category: number;
+}
+
+// Create your initial state
+const initialState: CategoriesState = {
+  categories: [],
+  category: 0,
+};
+
+export const categoriesSlice = createSlice({
+  name: "categories",
+  initialState,
+  reducers: {
+    getCategories: (state, action) => {
+      state.categories = action.payload.categories;
+    },
+    getCategory: (state, action) => {
+      state.category = action.payload;
+    },
+    // updatedCategory: (state, action) => {
+    //   state.categories = state.categories.map((cat) =>
+    //     cat.category_id === action.payload.category_id ? action.payload : cat
+    //   );
+    // },
+  },
+});
+
+// actions
+export const { getCategories, getCategory } = categoriesSlice.actions;
+
+// reducer
+export default categoriesSlice.reducer;

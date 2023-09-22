8facddc22f6ea8b7a50fc809d5029e4d60dd667a
bcnIslem <seifelislem.boucenina@univ-setif.dz>
2023-09-22 07:20:43 +0100

    added the states to the store.

---
+++ src/redux/store.ts
@@ -0,0 +1,39 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+// reducers
+import adminsReducer from "./admins";
+import usersReducer from "./users";
+import cartsReducer from "./carts";
+import wishlistsReducer from "./wishlists";
+import categoriesReducer from "./categories";
+import productsReducer from "./products";
+// orders and order details
+
+// persist config
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// combine reducers
+const rootReducer = combineReducers({
+  admins: adminsReducer,
+  users: usersReducer,
+  carts: cartsReducer,
+  wishlists: wishlistsReducer,
+  categories: categoriesReducer,
+  products: productsReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
+});
+
+export const persistor = persistStore(store);

266c1361db69a0c28f1fbe91fbb4c1d74cf6800f
bcnIslem <seifelislem.boucenina@univ-setif.dz>
2023-09-25 13:29:15 +0100

    added the latest state to the product slice.

--- src/redux/products.ts
+++ src/redux/products.ts
@@ -6,6 +6,7 @@ export const productsSlice = createSlice({
     products: [],
     productsByCategory: [],
     product: 0,
+    latest: 0,
   },
   reducers: {
     getProducts: (state, action) => {
@@ -17,11 +18,14 @@ export const productsSlice = createSlice({
     getProduct: (state, action) => {
       state.product = action.payload;
     },
+    getLatest: (state, action) => {
+      state.latest = action.payload;
+    },
   },
 });
 
 // actions
-export const { getProducts, getProductsByCategory, getProduct } =
+export const { getProducts, getProductsByCategory, getProduct, getLatest } =
   productsSlice.actions;
 
 // reducer
